147028182ac7d38568432478b0fa650e9803fffe
赵文祺 <1027572886a@gmail.com>
2020-02-23 20:47:31 +0800

    feat: docker-compose中对于es的支持

---
+++ init_es/import_es.py
@@ -0,0 +1,14 @@
+import json
+from elasticsearch import Elasticsearch
+
+with open('papers.json', 'r') as f:
+    papers = json.load(f)
+
+es = Elasticsearch(
+    ['es'],
+    port=9200
+)
+
+for p in papers:
+    es.index(index='se3', doc_type='papers', body=p)
+
