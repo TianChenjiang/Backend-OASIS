05a014d66640347045cf4f68336c961fd8273c9d
赵文祺 <1027572886a@gmail.com>
2020-02-24 14:51:44 +0800

    fix: 缓慢注入数据

--- init_es/import_es.py
+++ init_es/import_es.py
@@ -1,6 +1,7 @@
 import json
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
+import time
 
 with open('papers.json', 'r') as f:
     papers = json.load(f)
@@ -19,5 +20,10 @@ all_actions = [
     for i, p in enumerate(papers)
 ]
 
-helpers.bulk(es, all_actions)
+n = 5
+
+for i in range(0, len(all_actions), n):
+    action = all_actions[i: i+n]
+    helpers.bulk(es, action)
+    time.sleep(1)
 
