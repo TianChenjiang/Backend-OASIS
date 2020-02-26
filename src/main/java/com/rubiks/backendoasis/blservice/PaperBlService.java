303f99174acc89aa372cf4c3094f173a2606dacf
TianChenjiang <tiancj0310@outlook.com>
2020-02-26 12:05:50 +0800

    refactor: 修改两个搜索接口的名字

--- src/main/java/com/rubiks/backendoasis/blservice/PaperBlService.java
+++ src/main/java/com/rubiks/backendoasis/blservice/PaperBlService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PaperBlService {
     List<PaperDocument> findAll() throws Exception;
-    List<PaperDocument> basicSearch(String keyword, int page) throws Exception;
-    List<PaperDocument> advancedSearch(String author, String affiliation, String conferenceName, String keyword, int page)
+    List<PaperDocument> basicSearchByES(String keyword, int page) throws Exception;
+    List<PaperDocument> advancedSearchByES(String author, String affiliation, String conferenceName, String keyword, int page)
         throws Exception;
 }
