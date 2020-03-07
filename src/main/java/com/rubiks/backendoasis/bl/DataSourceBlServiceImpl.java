d2de0206f35bf397da5ebfd0db540d6aabe06a56
TianChenjiang <tiancj0310@outlook.com>
2020-03-08 02:24:32 +0800

    feat: 增加csv导入功能

--- src/main/java/com/rubiks/backendoasis/bl/DataSourceBlServiceImpl.java
+++ src/main/java/com/rubiks/backendoasis/bl/DataSourceBlServiceImpl.java
@@ -1,11 +1,11 @@
 package com.rubiks.backendoasis.bl;
 
-import com.mongodb.gridfs.GridFSDBFile;
 import com.rubiks.backendoasis.blservice.DataSourceBlService;
 import com.rubiks.backendoasis.entity.PaperEntity;
 import com.rubiks.backendoasis.exception.FileFormatNotSupportException;
 import com.rubiks.backendoasis.model.ImportPaperRes;
 import com.rubiks.backendoasis.response.BasicResponse;
+import com.rubiks.backendoasis.util.CSVConvertor;
 import com.rubiks.backendoasis.util.Constant;
 import com.rubiks.backendoasis.util.MultiPartFileToFile;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +19,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.rubiks.backendoasis.util.CSVConvertor.csvToJson;
 
 @Service
 public class DataSourceBlServiceImpl implements DataSourceBlService {
@@ -31,6 +38,7 @@ public class DataSourceBlServiceImpl implements DataSourceBlService {
     public BasicResponse importPaperData(MultipartFile file) {
 //        File f = (File) file;
         List<org.bson.Document> documents = new ArrayList<>();
+        int resSize = 0;
         try  {
             File f = MultiPartFileToFile.convert(file);
             BufferedReader br = new BufferedReader(new FileReader(f.getPath()));
@@ -40,28 +48,48 @@ public class DataSourceBlServiceImpl implements DataSourceBlService {
                 f.delete();
                 throw new FileFormatNotSupportException();
             }
-            f.delete(); // 删除转换而来的json文件
-            String line;
-            while ((line = br.readLine()) != null) {
-                org.bson.Document d = org.bson.Document.parse(line);
-                String doi = d.get("doi").toString();
-                Criteria criteria = Criteria.where("doi").is(doi);
-                Query query = new Query(criteria);
-                List<PaperEntity> paperEntities = mongoTemplate.find(query, PaperEntity.class);
-                if (paperEntities.size()!= 0) {
-                    continue;
-                } else {
-                    documents.add(d);
+
+            if (format.equals("csv")) {
+                List<PaperEntity> newPapers = new ArrayList<>();
+                for (PaperEntity p : CSVConvertor.csvToBean(f.getPath())) {
+                    String doi = p.getDoi();
+                    Criteria criteria = Criteria.where("doi").is(doi);
+                    Query query = new Query(criteria);
+                    List<PaperEntity> paperEntities = mongoTemplate.find(query, PaperEntity.class);
+                    if (paperEntities.size()!= 0) {
+                        continue;
+                    } else {
+                        newPapers.add(p);
+                    }
                 }
+                if (newPapers.size() != 0) {
+                    mongoTemplate.save(newPapers, Constant.collectionName);
+                }
+                resSize = newPapers.size();
+            } else {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    org.bson.Document d = org.bson.Document.parse(line);
+                    String doi = d.get("doi").toString();
+                    Criteria criteria = Criteria.where("doi").is(doi);
+                    Query query = new Query(criteria);
+                    List<PaperEntity> paperEntities = mongoTemplate.find(query, PaperEntity.class);
+                    if (paperEntities.size()!= 0) {
+                        continue;
+                    } else {
+                        documents.add(d);
+                    }
+                }
+                if (documents.size() != 0) {
+                    mongoTemplate.getCollection(Constant.collectionName).insertMany(documents);
+                }
+                resSize = documents.size();
             }
+            f.delete(); // 删除转换而来的json文件
         } catch (IOException ex) {
             ex.getMessage();
         }
-        if (documents.size() != 0) {
-            mongoTemplate.getCollection(Constant.collectionName).insertMany(documents);
-        }
-
-        return new BasicResponse(200, "Success", new ImportPaperRes(documents.size()));
+        return new BasicResponse(200, "Success", new ImportPaperRes(resSize));
     }
 }
 
---
+++ src/main/java/com/rubiks/backendoasis/util/CSVConvertor.java
@@ -0,0 +1,56 @@
+package com.rubiks.backendoasis.util;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+import com.rubiks.backendoasis.entity.PaperEntity;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.opencsv.CSVReader;
+
+public class CSVConvertor {
+
+    public static List<PaperEntity> csvToBean(String filePath) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("_id", "id");
+        map.put("title", "title");
+        map.put("authors__name", "authors.name");
+        map.put("abstract", "abstract");
+        map.put("publicationTitle", "publicationTitle");
+        map.put("doi", "doi");
+        map.put("publicationYear", "publicationYear");
+        map.put("conferenceName", "conferenceName");
+        map.put("link", "link");
+
+
+        HeaderColumnNameTranslateMappingStrategy<PaperEntity> strategy = new HeaderColumnNameTranslateMappingStrategy<PaperEntity>();
+        strategy.setType(PaperEntity.class);
+        strategy.setColumnMapping(map);
+
+        com.opencsv.CSVReader csvReader = null;
+        try {
+            csvReader = new com.opencsv.CSVReader(new FileReader(filePath));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        CsvToBean csvToBean = new CsvToBean();
+        csvToBean.setCsvReader(csvReader);
+        csvToBean.setMappingStrategy(strategy);
+
+        List<PaperEntity> list = csvToBean.parse();
+        List<PaperEntity> res  = new ArrayList<>();
+        for (PaperEntity e : list) {
+            if (e.getId() != null & !e.getId().isEmpty()) {
+                res.add(e);
+//                System.out.println(e);
+            }
+        }
+        return res;
+    }
+
+}
