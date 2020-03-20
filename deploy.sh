d74abe6bca35b7b63b728b08ce39e3290b607973
赵文祺 <1027572886a@gmail.com>
2020-03-20 18:08:06 +0800

    chore: 更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ git pull origin develop
 #docker-compose pull
 #docker-compose up -d
 
-docker-compose stop server
-docker-compose pull server
-docker-compose up -d server
+#docker-compose stop server
+#docker-compose pull server
+#docker-compose up -d server
+
+docker pull registry.cn-hangzhou.aliyuncs.com/rubiks-oasis/backend
+docker stop backend-oasis
+docker rm backend-oasis
+docker run -d --name=backend-oasis --restart=always -p 8081:8081 registry.cn-hangzhou.aliyuncs.com/rubiks-oasis/backend
